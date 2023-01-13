97be710d5b97ac480010e0ba10f6fec5a258814c
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-01-13 17:59:36 -0300

    instalación de heroku

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const cors = require('cors')
 const routerApi = require('./routes')
 
 const {logErrors, errorHandler, boomErrorHandler}= require('./middlewares/errorHandler')
+
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
+
+const whitelist =['http://localhost/8080', 'https://myapp.com']
+const options ={
+  origin: (origin, callback)=>{
+    if(whitelist.includes(origin)){
+      callback(null, true)
+    }else{
+      callback(new Error('no permitido') )
+    }
+
+  }
+}
+app.use(cors(options))
 
 app.get("/", (req, res) => {
   res.send('Hola mi server en express')
