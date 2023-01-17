74980f4d746ace8095acba6ffac72a61841d8f83
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-01-17 16:03:40 -0300

    conection with .env and config enviroment

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg')
-require('dotenv').config()
+const { config } = require('./../config/config')
+
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const pool = new Pool({
-  host: process.env.HOST,
-  por: process.env.PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  connectionString: URI
 });
 
 
