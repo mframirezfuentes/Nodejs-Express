903b75d3cdcb9e7e00404dc927c49e3270ffb197
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-05-11 17:43:16 -0400

    Creación del token dede el lado del servidor

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const {config} = require('../config/config')
+
+const secret = config.jwt
 
 
 
@@ -9,7 +13,17 @@ router.post("/login",
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
-      res.status(201).json(req.user)
+      const user = req.user
+      const payload = {
+        sub: user.id,
+        role: user.role
+      }
+      const token = jwt.sign(payload, secret)
+      res.status(201).json({
+        user,
+        token
+      })
+
     } catch (error) {
       next(error)
     }
